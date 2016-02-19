d48cf40a7159db0669ebfa7959a4d1310a17014d
Sam Krause <samuelnkrause@gmail.com>
2016-02-19 14:47:58 -0500

    merging sam and bills work still need to do finishing touches

---
+++ public/js/sinatrabusiness.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+ startingPoint();
+ function startingPoint(){
+   console.log("startingPoint firing");
+   $('.productBox').hide()
+
+   $('#products').click(function(){
+     $('.productBox').slideDown("slow")
+   })
+
+
+
+
+ }
+
+
+
+
+
+
+})
